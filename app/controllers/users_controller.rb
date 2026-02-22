2264e648d58a35dba1e068a8beccd51c30d3e158
gitYutaHarada <mayutokuyuta@gmail.com>
2026-02-22 22:30:41 +0900

    リスト10.19まで完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+  before_action -> {
+    if (user_id = session[:user_id])
+      if @current_user.nil?
+        @current_user = User.find_by(id: user_id)
+      elsif (user_id = cookies.encrypted[:user_id])
+        user = User.find_by(id: user_id)
+        if user && BCrypt::Password.new(user.remember_digest).is_password?(cookies[:remember_token])
+          sessions[:user_id] = user.id
+          @current_user = user
+        end
+      end
+    end
+    unless !@current_user.nil?
+      flash[:danger] = "Please log in"
+      redirect_to login_url, status: :see_other
+    end
+  }, only: [ :edit, :update ]
   def show
     @user = User.find(params[:id])
   end
@@ -21,5 +38,15 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(params.require(:user).permit(:name, :email, :password, :password_confirmation))
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render "edit", status: :unprocessable_entity
+    end
   end
 end
